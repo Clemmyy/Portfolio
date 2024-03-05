b4bb44b4a30ce124e8e6d035a7c5ec856fb0fcca
Clement <clementbabili23@gmail.com>
2024-03-05 16:05:40 +0200

    completed

---
+++ src/utils/dark-mode.js
@@ -0,0 +1,19 @@
+const themeToggleBtn = document.querySelectorAll('#theme-toggle')
+
+const theme = localStorage.getItem('theme')
+
+theme &&  document.body.classList.add(theme)
+
+const handleThemeToggle = () => {
+    document.body.classList.toggle('light-mode')
+        if (document.body.classList.contains('light-mode')) {
+            localStorage.setItem('theme', 'light-mode')
+        } else {
+            localStorage.removeItem('theme')
+            document.body.removeAttribute('class')
+        }
+}
+
+themeToggleBtn.forEach(btn => {
+    btn.addEventListener('click', handleThemeToggle)
+})
